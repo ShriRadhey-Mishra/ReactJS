60f94c0b2e6af57abf3f3ffa11ad8bf5fc352831
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-02-21 14:02:48 +0530

    done Props

--- Basics/01-starting-project/src/App.jsx
+++ Basics/01-starting-project/src/App.jsx
@@ -1,33 +1,27 @@
-import reactImg from "./assets/react-core-concepts.png";
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-    let description = reactDescriptions[genRandomInt(2)];
-    return (
-        <header>
-            <img src={reactImg} alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-                {description} React concepts you will need for almost any app you are going to build!
-            </p>
-        </header>
-    );
-}
+import { CORE_CONCEPTS } from "./data.js"
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts/CoreConcepts";
 
 function App() {
     return (
         <div>
             <Header />
             <main>
-                <h2>Time to get started!</h2>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcepts {...CORE_CONCEPTS[0]} />
+                        <CoreConcepts {...CORE_CONCEPTS[1]} />
+                        <CoreConcepts {...CORE_CONCEPTS[2]} />
+                        <CoreConcepts {...CORE_CONCEPTS[3]} />
+                    </ul>
+                </section>
             </main>
         </div>
     );
 }
 
 export default App;
+
+
+
